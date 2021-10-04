fbbd4f399addf0a612e91e18956817cb1c2588f7
mars-iq <91744592+mars-iq@users.noreply.github.com>
2021-10-05 00:45:39 +0300

    Update AmwalClass.php

--- src/classes/AmwalClass.php
+++ src/classes/AmwalClass.php
@@ -17,7 +17,7 @@ class AmwalClass extends MainClass
     $this->client = new Client(['base_uri' => $this->urlapi]);
   }
 
-  function getPay(float $amount,string $description,string $currency,string $returnUrl,string $failUrl,string $orderNum)
+  function getPay(float $amount, string $description, string $currency, string $returnUrl, string $failUrl, string $orderNum)
   {
     //   $randnum=rand(10000,99999);
     $url = $this->urlapi . "/payment/request";
@@ -32,26 +32,21 @@ class AmwalClass extends MainClass
       "callback" => $failUrl,
       "return" => $returnUrl
     );
-    $data = json_encode($data);
+    $body = json_encode($data);
+    $response = $this->client->request('POST',  $url, [
+      'body' => $body,
+      'headers' => [
+        'authorization' => $this->serverkey,
+        'Content-type'     => 'application/json',
 
-    $serverkey = $this->serverkey;
-    $header = array("authorization: " . $serverkey, "content-type: application/json");
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $response1 = curl_exec($ch);
- 
-    curl_close($ch);
-    $data = json_decode($response1, true);
-    if(isset($data['redirect_url'])){
-     
-        return array('url'=>$data['redirect_url'],'transiction_id'=>$data['tran_ref']);
-    }else{
-        return 0;
+      ]
+    ]);
+    $json_response = json_decode($response->getBody(), true);
+    if (isset($json_response['redirect_url'])) {
+
+      return array('status' => true, 'url' => $json_response['redirect_url'], 'transiction_id' => $json_response['tran_ref'], 'response' => $json_response);
+    } else {
+      return array('status' => false, 'response' => $json_response);
     }
-
-
   }
 }
