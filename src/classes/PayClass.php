d38e3a07919145302d8e2742de32099d1f9d0c3b
mars-iq <91744592+mars-iq@users.noreply.github.com>
2021-10-05 00:46:21 +0300

    Update PayClass.php

--- src/classes/PayClass.php
+++ src/classes/PayClass.php
@@ -1,30 +1,38 @@
 <?php
+
 namespace iqpay;
+
 require_once('main.php');
 require_once('ApsClass.php');
 require_once('AmwalClass.php');
 require_once('SwitchClass.php');
 require_once('TasdidClass.php');
 require_once('ZainCash.php');
+require_once('QiClass.php');
+
 use ApsClass;
 use AmwalClass;
 use SwitchClass;
 use ZainCashIQ\ZainCash;
 use TasdidClass;
+use QiClass;
+
 abstract class getWays
 {
     const zaincash = "zaincash";
     const aps = "aps";
     const amwal = "amwal";
     const tasdid = "tasdid";
+    const qi = "qi";
     const switch = "switch";
 }
-class PayClass 
+class PayClass
 {
     public float $amount;
     public string $currency;
     public  string $returnUrl;
     public string $failUrl;
+
     public string $language;
     public string $description;
     public string $method;
@@ -41,7 +49,7 @@ class PayClass
     public function __construct()
     {
     }
-    public function setData(string $nameCostumer,string $phoneCostumer,float $amount,string $orderNum,string $currency,string $returnUrl, string $failUrl,string $language,string $description,string $method)
+    public function setData(string $nameCostumer, string $phoneCostumer, float $amount, string $orderNum, string $currency, string $returnUrl, string $failUrl, string $language, string $description, string $method)
     {
         $this->nameCostumer = $nameCostumer;
         $this->phoneCostumer = $phoneCostumer;
@@ -58,24 +66,31 @@ class PayClass
     public function Pay()
     {
         if ($this->method == "aps") {
-           return $this->PayAps();
+            return $this->PayAps();
         } else if ($this->method == "switch") {
-return $this->PaySwitch();
+            return $this->PaySwitch();
         } else if ($this->method == "amwal") {
-          return  $this->PayAmwal();
+            return  $this->PayAmwal();
         } else if ($this->method == "zaincash") {
-         return   $this->PayZainCash();
+            return   $this->PayZainCash();
         } else if ($this->method == "tasdid") {
-
+        } else if ($this->method == "qi") {
+            return   $this->PayQi();
         }
     }
+    public function PayQi()
+    {
+        $QiClass = new QiClass();
+        $ret = $QiClass->getPay($this->amount, $this->currency, $this->orderNum, $this->returnUrl, $this->failUrl);
 
+        return $ret;
+    }
     public function PayAps()
     {
         $ApsClass = new ApsClass();
-       $ret = $ApsClass->getPay($this->amount, $this->orderNum, $this->currency, $this->returnUrl, $this->failUrl, $this->language, $this->description);
-  
-       return $ret;
+        $ret = $ApsClass->getPay($this->amount, $this->orderNum, $this->currency, $this->returnUrl, $this->failUrl, $this->language, $this->description);
+
+        return $ret;
     }
 
     public function PayZainCash()
@@ -84,17 +99,16 @@ return $this->PaySwitch();
             $zc = new ZainCash($this->returnUrl, $this->language);
 
 
-        $ret=  $zc->getPay(
+            $ret =  $zc->getPay(
                 $this->amount,
                 $this->description,
                 $this->orderNum
             );
-          
+
             return $ret;
-           
         } catch (Exception $e) {
-         echo $e->getMessage();
-            return 0;
+            echo $e->getMessage();
+            return array('status' => false);
         }
     }
     function PayAmwal()
@@ -102,29 +116,29 @@ return $this->PaySwitch();
 
         $AmwalClass = new AmwalClass();
 
-      return  $AmwalClass->getPay($this->amount, $this->description, $this->currency, $this->returnUrl, $this->failUrl, $this->orderNum);
+        return  $AmwalClass->getPay($this->amount, $this->description, $this->currency, $this->returnUrl, $this->failUrl, $this->orderNum);
     }
 
 
-    public function PaySwitch(){
-        $SwitchClass=new SwitchClass();
-        $ret=$SwitchClass->getPay( $this->amount, $this->cardNumber, $this->cardHolder, $this->currency,$this->Mounth, $this->Year, $this->Cvv);
+    public function PaySwitch()
+    {
+        $SwitchClass = new SwitchClass();
+        $ret = $SwitchClass->getPay($this->amount, $this->cardNumber, $this->cardHolder, $this->currency, $this->Mounth, $this->Year, $this->Cvv);
         return $ret;
     }
 
-    function checkOrder(String $orderNum)
+    function checkOrder(String $orderNum, string $transiction_id)
     {
         if ($this->method == "aps") {
             $ApsClass = new ApsClass();
             $ret = $ApsClass->CheckPay($orderNum);
-            if ($ret) {
-                return 1;
-            } else {
-                return 0;
-            }
+            return $ret;
+        } else if ($this->method == "qi") {
+            $QiClass = new QiClass();
+            $ret = $QiClass->CheckPay($orderNum, $transiction_id);
+            return $ret;
         } else {
             return -1;
         }
     }
-    
 }
